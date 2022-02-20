c079f814a2f60994af2afdc98c6b0cebfba4b7cc
KilledByCheese <ktoffelrofl@gmail.com>
2022-02-20 23:32:54 +0100

    added sql create statement to doc

--- doc/db_setup.sql
+++ doc/db_setup.sql
@@ -7,3 +7,8 @@ CREATE TABLE recipe_user (
 	pw_hash VARCHAR(60) not null,
 	user_role user_role_enum not null
 );
+
+CREATE TABLE category (
+	id serial PRIMARY KEY,
+	value VARCHAR (50) unique not NULL
+);
